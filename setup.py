0bb80186205bc99cbc3e6aa38127230e35d3856e
Rajesh Singh <rajeshrinet@gmail.com>
2020-05-31 13:45:35 +0100

    minor

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ from setuptools import setup, Extension
 
 
 setup(
-    name='pyGL',
+    name='pygl',
     version='1.0.0',
     url='https://github.com/rajeshrinet/pygl',
     author='The PyGL team',
     license='MIT',
     description='python library for numerical simulation of field theories',
+    long_description='PyGL is a library for numerical simulation of field theories\
+    in non-equilibrium statistical mechanics',
     platforms='works on all platforms (such as LINUX, macOS, and Microsoft Windows)',
     ext_modules=cythonize([ Extension("pygl/*", ["pygl/*.pyx"],
         include_dirs=[numpy.get_include()],
