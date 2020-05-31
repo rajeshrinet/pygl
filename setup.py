fbad1506ac650f28f86ef4ede507e149ef6aa2f6
Rajesh Singh <rajeshrinet@gmail.com>
2020-05-31 12:49:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@ Cython.Compiler.Options.annotate=True
 
 
 setup(
-    name='PyLandau',
+    name='pyGL',
     version='1.0.0',
-    url='https://github.com/rajeshrinet/pylandau',
-    author='The PyLandau team',
+    url='https://github.com/rajeshrinet/pygl',
+    author='The PyGL team',
     license='MIT',
     description='python library for numerical simulation of field theories',
     platforms='works on all platforms (such as LINUX, macOS, and Microsoft Windows)',
-    ext_modules=cythonize([ Extension("pylandau/*", ["pylandau/*.pyx"],
+    ext_modules=cythonize([ Extension("pygl/*", ["pygl/*.pyx"],
         include_dirs=[numpy.get_include()],
         )],
         compiler_directives={"language_level": sys.version_info[0]},
         ),
     libraries=[],
-    packages=['pylandau'],
-    package_data={'pylandau': ['*.pxd']},
+    packages=['pygl'],
+    package_data={'pygl': ['*.pxd']},
 )
